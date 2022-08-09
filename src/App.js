2a09eb8c5df0f72a7868188adab31c240893511d
Henry <henryhein.hein@gmail.com>
2022-08-09 05:43:12 -0700

    ranked and filtered Top10 banana counts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
+import { useState } from 'react';
 import './App.css';
 import Leaderboard from './data/leaderboard.json';
 
 const App = () => {
+
+  const [searchInput, setSearchInput] = useState('');
+ 
+  // Sort converts number into string so instead we execute compareFunction. Thereby, comparing return value of the compare function instead, which in our case is typeOf Number.
+  // If a > b sort a after b
+  // If b > a sort b after a
+  // If a = b do not change order
+  // return b-a to sort in descending order
+  // here we sort by numbers of bananas
+
+  const sortDescending = Object.values(Leaderboard).splice('').sort((a, b) => {
+    return b.bananas - a.bananas;
+  });
+
+  // Modified sortDescending Array of objects and inserted a rank property
+  const rankedArray = () => {
+    let rank = 1;
+    for (let i = 0; i < sortDescending.length; i++) {
+    // increase rank only if current bananas are less than previous
+    if (i > 0 && sortDescending[i].bananas < sortDescending[i - 1].bananas) {
+      rank++;
+    }
+      sortDescending[i].rank = rank;
+    } 
+  }
+  rankedArray(); //sortDescending is ranked
+
+  const top10BananasCount = [...sortDescending].slice(0,10); //Create new array limited to people with top 10 bananas
+
+  const onClick = () => {
+    // If uid matches searchInput returns the found object sortDescending Array
+    const findPersonById = sortDescending.find((person) => {
+      if(searchInput !== person.uid){
+        return;
+      }
+      return person.uid === searchInput;
+    });
+    console.log(findPersonById);
+  }
+ 
   return (
     <div className="container">
-      <div className="input-group mb-3 mt-5">
-        <input type="text" className="form-control" placeholder="Insert User ID"/>
-        <button className="btn btn-outline-primary" type="button">Search</button>
-      </div>
-
+      <form className="input-group mb-3 mt-5">
+        <input type="text" className="form-control" placeholder="Insert User ID" onChange={ (e) => {
+            setSearchInput(e.target.value);
+          }}
+        />
+        <button className="btn btn-outline-primary" type="button" onClick={onClick}>
+          Search
+        </button>
+      </form>
       <table className="table">
         <thead>
           <tr>
@@ -19,21 +64,17 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-          {Object.values(Leaderboard).map((data) => {
-            // console.log(data.name);
+          {top10BananasCount.map((data) => {
             return (
-              <tr key={data.uid}>
-                <td>{data.name}</td>
-                <td>Rank</td>
-                <td>{data.bananas}</td>
-                <td>Rank</td>
-              </tr>
+                <tr key={data.uid}>
+                  <td>{data.name}</td>
+                  <td>{data.rank}</td>
+                  <td>{data.bananas}</td>
+                  <td>Rank</td>
+                </tr>
             );
-          })}
-          
-        </tbody>
-        
-        
+          })}          
+        </tbody>  
       </table>
     </div>
   );
